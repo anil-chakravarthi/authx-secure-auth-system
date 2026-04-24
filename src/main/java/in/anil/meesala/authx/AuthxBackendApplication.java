9fe29709bfc694dedd5f2319fdf1972bfe9304d1
Dileep kumar Meesala <r170988@rguktrkv.ac.in>
2026-04-24 11:27:55 +0530

    Initial AuthX backend setup with Spring Boot

---
+++ src/main/java/in/anil/meesala/authx/AuthxBackendApplication.java
@@ -0,0 +1,13 @@
+package in.anil.meesala.authx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/anil/meesala/authx/AuthxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.anil.meesala.authx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
